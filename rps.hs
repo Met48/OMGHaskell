595b0932509f68a322f03a45f197b6594da4a5db
Andrew Sutton <met48@met48.com>
2013-04-10 20:06:54 -0700

    Rock Paper Scissors

---
+++ rps.hs
@@ -0,0 +1,25 @@
+import Control.Applicative
+
+data Hand = Rock | Paper | Scissors deriving (Show, Eq, Read)
+
+instance Ord Hand where
+    Rock <= Paper = True
+    Paper <= Scissors = True
+    Scissors <= Rock = True
+    a <= b = not $ b <= a
+
+getResult :: Hand -> Hand -> String
+getResult hand1 hand2 = do
+    let
+        comp = hand1 `compare` hand2
+    case comp of
+        GT -> "Player one wins"
+        LT -> "Player two wins"
+        EQ -> "Tie"
+
+getInput :: IO Hand
+getInput = do
+    putStrLn "Enter Rock Paper or Scissors:"
+    getLine >>= return . read
+
+main = getResult <$> getInput <*> getInput >>= putStrLn
